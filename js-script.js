df0c61c2f49b2a484f2ff3cdd14be2da4bf72b59
ecesarini <73652902+ecesarini@users.noreply.github.com>
2021-11-29 23:56:23 +0100

    Update js-script.js

--- js-script.js
+++ js-script.js
@@ -1,31 +1,18 @@
 function descNavbarClick() {
-  let buttonClick = document.getElementsByClassName("desc-navbar-button");
+  let buttonClickOnNavBar = document.getElementsByClassName("desc-navbar-button");
   
-  for (let i = 0; i < buttonClick.length; i++) {
-    buttonClick[i].addEventListener("click", function() {
+  for (let i = 0; i < buttonClickOnNavBar.length; i++) {
+    buttonClickOnNavBar[i].addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
       if (content.style.display === "block") {
         content.style.display = "none";
       } else {
         content.style.display = "block";
+        // content.firstChild
       }    
     });
   }
 }
 
 descNavbarClick();
-
-
-
-/*You can invite up to 2 additional users on the Free plan. There is no limit on 
-  team members for the Premium plan.
-
-  No more than 2GB. All files in your account must fit your allotted storage space.
-
-  Click “Forgot password” from the login page or “Change password” from your profile page.
-  A reset link will be emailed to you.
-
-  Yes! Send us a message and we’ll process your request no questions asked.
-
-  Chat and email support is available 24/7. Phone lines are open during normal business hours.*/
