534d8eb7703ab8679addeb9819f08791b471b650
ecesarini <73652902+ecesarini@users.noreply.github.com>
2021-12-16 23:03:45 +0100

    try to focus on click

--- js-script.js
+++ js-script.js
@@ -1,18 +1,24 @@
 function descNavbarClick() {
   let buttonClickOnNavBar = document.getElementsByClassName("desc-navbar-button");
-  
   for (let i = 0; i < buttonClickOnNavBar.length; i++) {
     buttonClickOnNavBar[i].addEventListener("click", function() {
+    isClicked[i] = true;
+      if (isClicked[i] ) {
+        this.style.color = "black";
+        this.style.fontWeight = 800;
+      }
+      isClicked[i] = false;
       this.classList.toggle("active");
       var content = this.nextElementSibling;
       if (content.style.display === "block") {
         content.style.display = "none";
       } else {
         content.style.display = "block";
-        // content.firstChild
       }    
+    //return isClicked;
     });
   }
+  return isClicked;
 }
 
-descNavbarClick();
+descNavbarClick(clicked, clickOnNavBar);
