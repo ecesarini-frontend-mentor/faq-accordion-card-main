d4e7e0ec801af166acf6f56185f9f87fff11db42
ecesarini <73652902+ecesarini@users.noreply.github.com>
2021-12-17 21:29:03 +0100

    Challenge update

--- js-script.js
+++ js-script.js
@@ -1,24 +1,20 @@
 function descNavbarClick() {
   let buttonClickOnNavBar = document.getElementsByClassName("desc-navbar-button");
+  let dnbColor = "black";
+  let dnbFw = 800;
   for (let i = 0; i < buttonClickOnNavBar.length; i++) {
     buttonClickOnNavBar[i].addEventListener("click", function() {
-    isClicked[i] = true;
-      if (isClicked[i] ) {
-        this.style.color = "black";
-        this.style.fontWeight = 800;
-      }
-      isClicked[i] = false;
       this.classList.toggle("active");
       var content = this.nextElementSibling;
       if (content.style.display === "block") {
         content.style.display = "none";
+        this.removeAttribute("style");
       } else {
         content.style.display = "block";
+        this.style.color = dnbColor;
+        this.style.fontWeight = dnbFw;
       }    
-    //return isClicked;
     });
   }
-  return isClicked;
 }
-
-descNavbarClick(clicked, clickOnNavBar);
+descNavbarClick();
